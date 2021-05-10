4565aef311938a72b1317baac861d1d38921ac9b
Raphael Ramalho <rsramalhovf@gmail.com>
2021-05-10 09:06:39 -0300

    update

--- 1-meuPrimeiroPrograma.js
+++ 1-meuPrimeiroPrograma.js
@@ -1,1 +1,11 @@
-console.log("Meu primeiro programa");
+console.log("Meu primeiro programa: trabalhahndo com variáveis");
+
+const idade = 26;
+
+console.log (idade);
+console.log (idade + 2);
+
+const idadeSomada = idade + 2;
+console.log(idadeSomada);
+console.log(idadeSomada/2);
+
