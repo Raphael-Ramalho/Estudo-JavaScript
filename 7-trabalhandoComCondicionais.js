d74c2c8734eeb97584def57b2d3cce0e57260c17
Raphael Ramalho <rsramalhovf@gmail.com>
2021-05-10 12:29:12 -0300

    Update 7-trabalhandoComCondicionais.js

--- 7-trabalhandoComCondicionais.js
+++ 7-trabalhandoComCondicionais.js
@@ -1,19 +1,23 @@
 console.log(`Trabalhando com condicionais`);
 
 const listaDeDestinos = new Array(
-    `Salvador`, 
+    `Salvador`,
     `São Paulo`,
     `Rio de Janeiro`,
 );
 
 const idadeComprador = 15;
+const estaAcompanhada = true;
 
 console.log(`Destinos possíveis`);
 console.log(listaDeDestinos);
 
-if (idadeComprador >= 18){
+if (idadeComprador >= 18) {
     console.log(`Comprador maior de idade`);
-    listaDeDestinos.splice(1,1);
+    listaDeDestinos.splice(1, 1);
+} else if (estaAcompanhada) {
+    console.log(`Comprador está acompanhado`);
+    listaDeDestinos.splice(1, 1);
 } else {
     console.log(`Comprador não é maior de idade e não posso vender.`);
 }
