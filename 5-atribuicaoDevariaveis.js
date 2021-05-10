d0b6f2523ea370c6c96990014f150f382d995eac
Raphael Ramalho <rsramalhovf@gmail.com>
2021-05-10 11:13:13 -0300

    Update 5-atribuicaoDevariaveis.js

--- 5-atribuicaoDevariaveis.js
+++ 5-atribuicaoDevariaveis.js
@@ -1,1 +1,13 @@
 console.log("Trabalhandocom atribuição de variáveis");
+
+const idade = 29;
+const primeiroNome = "Ricardo";
+const sobrenome = "Bugan";
+
+console.log(nome + " " + sobrenome);
+
+console.log(`Meu nome é ${nome} ${sobrenome}`);
+
+const nomeCompleto = primeiroNome + sobrenome;
+
+console.log(nomeCompleto);
