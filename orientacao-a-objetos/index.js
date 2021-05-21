96e5883516e5bba7bb28f1c6a389cb5e74c52603
Raphael Ramalho <rsramalhovf@gmail.com>
2021-05-21 20:27:06 -0300

    update

---
+++ orientacao-a-objetos/index.js
@@ -0,0 +1,20 @@
+class Client{
+    nome;
+    cpf;
+    agencia;
+    saldo;
+}
+
+const cliente1 = new Client();
+
+cliente1.nome = "Ricardo";
+cliente1.cpf = 11122233309;
+cliente1.agencia = 1001;
+cliente1.saldo = 0;
+
+const cliente2Nome = "Alice";
+const cliente2cpf = 88822233309;
+const cliente2Agencia = 1001;
+const cliente2Saldo = 0;
+
+console.log(cliente1);
