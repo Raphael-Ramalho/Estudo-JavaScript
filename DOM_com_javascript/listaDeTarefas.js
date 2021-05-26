0615d0d2567046820f1497e6b24a46e01b2eec2c
Raphael Ramalho <rsramalhovf@gmail.com>
2021-05-26 14:16:53 -0300

    update

--- DOM_com_javascript/listaDeTarefas.js
+++ DOM_com_javascript/listaDeTarefas.js
@@ -1,9 +1,17 @@
 const criarTarefa = (evento) => {
     evento.preventDefault();
+
+    const lista = document.querySelector("[data-list]");
     const input = document.querySelector("[data-form-input]");
     const valor = input.value;
 
-    console.log(valor);
+    const tarefa = document.createElement("li");
+    const conteudo = `<p class = "content">${valor}</p>`
+
+    tarefa.innerHTML = conteudo;
+
+    lista.appendChild(tarefa);
+    input.value = " ";
 }
 
 const novaTarefa = document.querySelector("[data-form-button]");
