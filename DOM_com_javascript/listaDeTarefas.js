bae1d0fe653d96627ce399320385c2e1ee2df53f
Raphael Ramalho <rsramalhovf@gmail.com>
2021-05-28 14:36:01 -0300

    Update listaDeTarefas.js

--- DOM_com_javascript/listaDeTarefas.js
+++ DOM_com_javascript/listaDeTarefas.js
@@ -9,19 +9,25 @@ const criarTarefa = (evento) => {
     tarefa.classList.add("task");
     const conteudo = `<p class = "content">${valor}</p>`;
 
-    //tarefa.innerHTML = conteudo;
+    tarefa.innerHTML = conteudo;
 
+    tarefa.appendChild(BotaoConclui());
     lista.appendChild(tarefa);
-
-    //minha tentativa de resolução
-    const texto = document.querySelector(".task");
-    const elemento = document.createElement(`<p class = "content">${valor}</p>`);
-    texto.appendChild(elemento);
-    //
-
     input.value = " ";
 }
 
 const novaTarefa = document.querySelector("[data-form-button]");
 
 novaTarefa.addEventListener("click", criarTarefa);
+
+const BotaoConclui = () => {
+    const botaoConclui = document.createElement("button");
+
+    botaoConclui.classList.add("check-button");
+    botaoConclui.innerText = "concluir";
+    botaoConclui.addEventListener("click", () =>{
+        console.log("Fui clicado");
+    })
+
+    return botaoConclui;
+}
