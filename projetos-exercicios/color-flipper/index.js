60fdd771f24c59883afbf821dc072b8198504178
Raphael Ramalho <rsramalhovf@gmail.com>
2021-06-17 17:24:35 -0300

    update

--- projetos-exercicios/color-flipper/index.js
+++ projetos-exercicios/color-flipper/index.js
@@ -2,17 +2,16 @@ const button = document.querySelector("[data-button]");
 
 
 
-let color = () => {
+const color = () => {
 
-    let conjunto = [];
+    const conjunto = [];
 
     for (i=0; i <= 2; i++){
-        let numAleatorio = Math.floor(256 * Math.random());
+        const numAleatorio = Math.floor(256 * Math.random());
         conjunto[i] = numAleatorio;
     }
 
     document.body.style.backgroundColor = `rgb(${conjunto})`;
-
 }
 
 
