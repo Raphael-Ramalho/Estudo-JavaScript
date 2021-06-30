7d52d37d405f3df678bcb4fe45bebf4f6b836c62
Raphael Ramalho <rsramalhovf@gmail.com>
2021-06-30 16:09:31 -0300

    update

--- DOM_com_javascript/componentes/deletaTarefa.js
+++ DOM_com_javascript/componentes/deletaTarefa.js
@@ -1,12 +1,16 @@
-const deletaTarefa = () =>{
-    
+const deletaTarefa = (atualiza, id) =>{
+    const index = id
+    const tarefasCadastradas = JSON.parse(localStorage.getItem("tarefas"))
+    tarefasCadastradas.splice(index, 1)
+    localStorage.setItem("tarefas", JSON.stringify(tarefasCadastradas))
+    atualiza()
 }
 
-const BotaoDeleta = () => {
+const BotaoDeleta = (atualiza, id) => {
     const botaoDeleta = document.createElement("button");
 
     botaoDeleta.innerText = "Deletar";
-    botaoDeleta.addEventListener("click", deletaTarefa);
+    botaoDeleta.addEventListener("click", () => deletaTarefa(atualiza, id));
 
     return botaoDeleta;
 }
