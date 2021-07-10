55c458556abaf7e34eafeca0830d8bffe2e647cf
Raphael Ramalho <rsramalhovf@gmail.com>
2021-07-10 18:56:43 -0300

    update

--- CRUD-com-js-assincrono/services/cliente-service.js
+++ CRUD-com-js-assincrono/services/cliente-service.js
@@ -1,20 +1,4 @@
-const criaNovaLinha = (nome, email) => {
-    const linhaNovoCliente = document.createElement('tr')
-    const conteudo = `
-    <td class="td" data-td>${nome}</td>
-                <td>${email}</td>
-                <td>
-                    <ul class="tabela__botoes-controle">
-                        <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
-                        <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
-                    </ul>
-                </td>
-                `
-    linhaNovoCliente.innerHTML = conteudo
-    return linhaNovoCliente
-}
 
-const tabela = document.querySelector("[data-tabela]")
 
 //bloco de coneção com API
 const listaClientes = () => {
@@ -25,10 +9,4 @@ const listaClientes = () => {
     })
 }
 
-//Execução da função listaClientes
-listaClientes()
-.then(data => {
-    data.forEach(elemento => {
-        tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email))
-    })
-})
+
