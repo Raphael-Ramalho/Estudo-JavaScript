b294aff954bda1819b50418fc09d8bb9f8954e07
Raphael Ramalho <rsramalhovf@gmail.com>
2021-07-10 22:07:22 -0300

    Update listaClientes-controller.js

--- CRUD-com-js-assincrono/controller/listaClientes-controller.js
+++ CRUD-com-js-assincrono/controller/listaClientes-controller.js
@@ -22,7 +22,7 @@ const tabela = document.querySelector("[data-tabela]")
 tabela.addEventListener("click", (evento)=>{
     let ehbotaoDeletar = evento.target.className === "botao-simples botao-simples--excluir" //identifica o elemento dentro do target que possua a classe especificada. O === é um condicional q vai gerar um True ou um False
     if(ehbotaoDeletar){
-        const linhaCliente = evento.target.closest("[data-id]")//essa linha está especificando a tr pai do target
+        const linhaCliente = evento.target.closest("[data-id]")//essa linha está especificando a tr pai do target. o metodo closest retorna o ancestral mais proximo, em relação ao elemento atual. No caso de o elemento atual possuir o seletor, o mesmo é retornado. Caso não exista um ancestral o metodo retorna null
         let id = linhaCliente.dataset.id
         clienteService.removeCliente(id)
         .then(()=>{
