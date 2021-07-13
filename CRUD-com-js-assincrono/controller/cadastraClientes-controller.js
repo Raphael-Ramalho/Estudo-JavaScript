44fc3375d37e7dbeb377a087761f59b2a7b5698f
Raphael Ramalho <rsramalhovf@gmail.com>
2021-07-13 19:44:09 -0300

    update

--- CRUD-com-js-assincrono/controller/cadastraClientes-controller.js
+++ CRUD-com-js-assincrono/controller/cadastraClientes-controller.js
@@ -13,7 +13,7 @@ formulario.addEventListener("submit", async(evento)=>{
         window.location.href = '../telas/cadastro_concluido.html' //window - tela inteira, location - onde vc está, href - para onde vai enviar
     }
     catch(error){
-        console.log(erro)
+        console.log(error)
         window.location.href = "../telas/erro.html"
     }
 })
