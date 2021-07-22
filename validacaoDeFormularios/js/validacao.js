324205adb0a53cb3821bcf016586e274f5ca835d
Raphael Ramalho <rsramalhovf@gmail.com>
2021-07-22 00:08:06 -0300

    Update validacao.js

--- validacaoDeFormularios/js/validacao.js
+++ validacaoDeFormularios/js/validacao.js
@@ -81,7 +81,7 @@ function validaCPF(input) {
     const cpfFormatado = input.value.replace(/\D/g, '') //a regex e o metodo replace estão capturando tudo o que não é digito e substituindo por uma string vazia
     let mensagem = ""
 
-    if(!chegaCPFRepetidos(cpfFormatado)){
+    if(!chegaCPFRepetidos(cpfFormatado) || !checaEstruturaCPF(cpfFormatado)){
         mensagem = "O CPF digitado não é valido."
     }
 
@@ -114,3 +114,32 @@ function chegaCPFRepetidos(cpf){
 }
 
 //verificação de validade de cpf por calculos matematicos.
+
+function checaEstruturaCPF(cpf){
+    const multiplicador = 10
+
+    return checaDigitoVerificador(cpf, multiplicador)
+}
+
+function checaDigitoVerificador(cpf, multiplicador){
+    if(multiplicador >= 12) {
+        return true
+    }
+    let multiplicadorInicial = multiplicador
+    let soma = 0
+    const cpfSemDigitos = cpf.substr(0, multiplicador - 1).split("")
+    const digitoVerificador = cpf.charAt(multiplicador - 1)
+    for(let contador = 0; multiplicadorInicial > 1; multiplicadorInicial--){
+        soma = soma + cpfSemDigitos[contador] * multiplicadorInicial
+        contador++
+    }
+    if(digitoVerificador == confirmaDigito(soma)){
+        return checaDigitoVerificador(cpf, multiplicador + 1)
+    }
+
+    return false
+}
+
+function confirmaDigito(soma){
+    return 11 - (soma % 11)
+}
